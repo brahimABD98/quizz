f601612a4d62781fec245b649e9fb38718ad433b
brahimABD98 <brahim.abderrahim@esprit.tn>
2023-09-29 21:11:16 +0100

    initial commit

---
+++ src/main/java/tn/lastdance/quizz/QuizzApplication.java
@@ -0,0 +1,12 @@
+package tn.lastdance.quizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuizzApplication.class, args);
+    }
+
+}
